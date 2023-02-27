d25a0e2f43e9dfe7fac0dfb270788e20e581bcb3
gonzallito <goncaloferreira2408@gmail.com>
2023-02-27 16:50:43 +0000

    :cookie:

--- cookies.js
+++ cookies.js
@@ -1,3 +1,6 @@
+
+/* SHOW POP UP */
+
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const overlay = document.getElementById('overlay')
 
@@ -26,4 +29,19 @@ function closeModal(modal) {
   if (modal == null) return
   modal.classList.remove('active')
   overlay.classList.remove('active')
+}
+
+
+
+/* Save Preferences ALERT */
+
+function savePreferences() {
+  var preferences = {
+    strictlyNecessary: document.getElementsByName('strictly-necessary')[0].checked,
+    preferences: document.getElementsByName('preferences')[0].checked,
+    statistics: document.getElementsByName('statistics')[0].checked,
+    marketing: document.getElementsByName('marketing')[0].checked,
+  };
+  document.cookie = 'preferences=' + JSON.stringify(preferences);
+  alert('Your preferences have been saved.');
 }
