a1e7a9d0fcbb0c96ec5a7c6882aeffcda4ad81ff
gonzallito <goncaloferreira2408@gmail.com>
2023-02-27 16:03:26 +0000

    The Beningin

---
+++ cookies.js
@@ -0,0 +1,29 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
